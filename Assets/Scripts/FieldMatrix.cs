488327dea497016b5506eb0b762ce08a2857c27e
Claudia Yan <claudiab.yan@gmail.com>
2016-10-14 19:41:26 -0400

    Added FieldMatrix setup script and testscene with a cube

---
+++ Assets/Scripts/FieldMatrix.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FieldMatrix : MonoBehaviour {
+    private bool[,] field;
+    public bool[,] playingfield { get { return field; } }
+    public int x;
+    public int z;
+
+         
+	// Use this for initialization
+	void OnEnable () {
+        field = new bool[x, z];    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
