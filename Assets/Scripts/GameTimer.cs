d7c121e5a116c04726e2cb17fbcbde97094bdfb8
Claudia Yan <claudiab.yan@gmail.com>
2016-10-15 10:50:08 -0400

    set timer value to private

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class GameTimer : MonoBehaviour {
     [SerializeField]
-    public float timer = 0f;
+    private float timer = 0f;
 
 	// Use this for initialization
 	void Start () {
