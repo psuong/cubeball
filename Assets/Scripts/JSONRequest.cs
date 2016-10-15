9614d56959be7c4f791fb6b998783e3b1caeb5b6
Porrith Suong <psuong95@gmail.com>
2016-10-15 14:27:08 -0400

    Tests to see if the WWW URL works.

---
+++ Assets/Scripts/JSONRequest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JSONRequest : MonoBehaviour 
+{
+	[Tooltip("How often do we ping the server?")]
+	public float intervalTimer = 2f;
+
+	private float internalTimer;
+	[SerializeField]
+	private string URL;
+	private WWW wwwObject;
+
+	private Coroutine urlRequest;
+
+	private void Start()
+	{
+		internalTimer = intervalTimer;
+	}
+
+	private void Update()
+	{
+		internalTimer -= Time.deltaTime;
+		if (internalTimer < 0)
+		{
+			// TODO: Reset the timer
+			// TODO: Request the URL
+		}
+	}
+
+	private IEnumerator RequestURL()
+	{
+		wwwObject = new WWW(URL);
+		yield return wwwObject;
+
+		Debug.LogFormat("JSON: {0}", wwwObject.text);
+	}
+}
