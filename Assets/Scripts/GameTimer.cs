49836703e99f85c6bd36689170c933783ba009e8
Claudia Yan <claudiab.yan@gmail.com>
2016-10-14 23:37:28 -0400

    Added in gametimer and event for when round time is reached

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour {
+    [SerializeField]
+    public float timer = 0f;
+    public float roundTime = 180f;
+
+    public delegate void TimeUpHandler();
+    public static event TimeUpHandler TimeUpEvent;
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("gameTimer");
+	}
+
+    public static void InvokeTimesUp()
+    {
+        if(TimeUpEvent != null)
+        {
+            TimeUpEvent();
+        }
+    }
+
+    private IEnumerator gameTimer()
+    {
+        yield return new WaitForSeconds(1f);
+        timer += 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (timer == roundTime)
+        {
+            InvokeTimesUp();
+        }
+	
+	}
+}
