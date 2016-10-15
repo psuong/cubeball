e30a7ae71027f9919988677c06444e55421565f4
Claudia Yan <claudiab.yan@gmail.com>
2016-10-15 15:31:32 -0400

    Added in Field Generator - creates a field with a surrounding red buffer area

---
+++ Assets/Scripts/FieldGenerator.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldGenerator : MonoBehaviour {
+    private int x;
+    private int z;
+    public GameObject fieldSquare;
+
+    void OnEnable()
+    {
+        x = GetComponent<FieldMatrix>().x;
+        z = GetComponent<FieldMatrix>().z;
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        SetupField();
+	
+	}
+
+    private void SetupField()
+    {
+        for(int i = 0; i < x; i ++)
+        {
+            for(int j = 0; j <=z; j++)
+            {
+                var tile = Instantiate(fieldSquare);
+                tile.transform.position = new Vector3(i, 0, j);
+            }
+        }
+        for(int zplace = -1; zplace<= z+1; zplace++)
+        {
+            var outterTile = Instantiate(fieldSquare);
+            var renderer = outterTile.GetComponent<Renderer>();
+            outterTile.transform.position = new Vector3(-1f, 0f, zplace);
+
+            var otherOutterTile = Instantiate(fieldSquare);
+            otherOutterTile.transform.position = new Vector3(x, 0f, zplace);
+            var otherRenderer = otherOutterTile.GetComponent<Renderer>();
+            renderer.material.color = Color.red;
+            otherRenderer.material.color = Color.red;
+        }
+
+        for(int xplace = -1;xplace <x; xplace++)
+        {
+            var outTile = Instantiate(fieldSquare);
+            var outRenderer = outTile.GetComponent<Renderer>();
+            outTile.transform.position = new Vector3(xplace, 0f, -1f);
+            outRenderer.material.color = Color.red;
+            var outTile2 = Instantiate(fieldSquare);
+            var outRenderer2 = outTile2.GetComponent<Renderer>();
+            outTile2.transform.position = new Vector3(xplace, 0f, z+1);
+            outRenderer2.material.color = Color.red;
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
