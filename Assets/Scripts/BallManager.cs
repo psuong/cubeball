7578f25dd097cedc007a89ec70a35d54438eec94
Porrith Suong <psuong95@gmail.com>
2016-10-15 19:32:41 -0400

    Almost final commit :D

--- Assets/Scripts/BallManager.cs
+++ Assets/Scripts/BallManager.cs
@@ -2,56 +2,62 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class BallManager : MonoBehaviour 
+public class BallManager : MonoBehaviour
 {
 	public float ballTimer = 2f;
 	public Vector3 spawnPosition;
-
+	public GameObject ball;
 	[SerializeField]
 	private GoalEvent goalEvent;
 
 	private Queue<GameObject> ballQueue;
 	private Coroutine routine;
 
-	private void Start()
+	private void Start ()
 	{
-		ballQueue = new Queue<GameObject>();
+		ballQueue = new Queue<GameObject> ();
+
+		var fieldMatrix = GameObject.FindObjectOfType<FieldMatrix> ();
+		Instantiate (ball, new Vector3 (fieldMatrix.x / 2, 0.5f, fieldMatrix.z / 2), Quaternion.identity);
+
+		spawnPosition.x = fieldMatrix.x / 2;
+		spawnPosition.y = 0.5f;
+		spawnPosition.z = fieldMatrix.z / 2;
 	}
 
-	private void OnEnable()
+	private void OnEnable ()
 	{
-		goalEvent.Subscribe(EnQueue);
+		goalEvent.Subscribe (EnQueue);
 	}
 
-	private void OnDisable()
+	private void OnDisable ()
 	{
-		goalEvent.Unsubscribe(EnQueue);
+		goalEvent.Unsubscribe (EnQueue);
 	}
 
-	private void EnQueue(GameObject gameObject)
+	private void EnQueue (GameObject gameObject)
 	{
 		// Disable the gameObject
-		gameObject.SetActive(false);
+		gameObject.SetActive (false);
 
 		// Move the gameObject back to the spawn position
 		gameObject.transform.position = spawnPosition;
-		ballQueue.Enqueue(gameObject);
-		DeQueue();
+		ballQueue.Enqueue (gameObject);
+		DeQueue ();
 	}
 
-	private void DeQueue()
+	private void DeQueue ()
 	{
-		if (routine == null)
-		{
-			routine = StartCoroutine(BallTimer());
+		if (routine == null) {
+			routine = StartCoroutine (BallTimer ());
 		}
 	}
 
-	private IEnumerator BallTimer()
+	private IEnumerator BallTimer ()
 	{
-		yield return new WaitForSeconds(ballTimer);
-		var ball = ballQueue.Dequeue();
-		ball.SetActive(true);
+		yield return new WaitForSeconds (ballTimer);
+		var ball = ballQueue.Dequeue ();
+		ball.SetActive (true);
 
 		routine = null;
 	}
--- Assets/Scripts/FieldGenerator.cs
+++ Assets/Scripts/FieldGenerator.cs
@@ -1,65 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FieldGenerator : MonoBehaviour {
-    private int x;
-    private int z;
-    public GameObject fieldSquare;
+public class FieldGenerator : MonoBehaviour
+{
+	private int x;
+	private int z;
+	public GameObject fieldSquare;
 
-    void OnEnable()
-    {
-        x = GetComponent<FieldMatrix>().x;
-        z = GetComponent<FieldMatrix>().z;
-    }
+	void OnEnable ()
+	{
+		x = GetComponent<FieldMatrix> ().x;
+		z = GetComponent<FieldMatrix> ().z;
+	}
 
 
 	// Use this for initialization
-	void Start () {
-        SetupField();
+	void Start ()
+	{
+		SetupField ();
 	
 	}
 
-    private void SetupField()
-    {
-        for(int i = 0; i < x; i ++)
-        {
-            for(int j = 0; j <=z; j++)
-            {
-                var tile = Instantiate(fieldSquare);
-                var fieldRenderer = tile.GetComponent<Renderer>();
-                fieldRenderer.material.color = Color.green;
-                tile.transform.position = new Vector3(i, 0, j);
-            }
-        }
-        for(int zplace = -1; zplace<= z+1; zplace++)
-        {
-            var outterTile = Instantiate(fieldSquare);
-            var renderer = outterTile.GetComponent<Renderer>();
-            outterTile.transform.position = new Vector3(-1f, 0f, zplace);
+	private void SetupField ()
+	{
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j <= z; j++) {
+				var tile = Instantiate (fieldSquare);
+				var fieldRenderer = tile.GetComponent<Renderer> ();
+				fieldRenderer.material.color = Color.green;
+				tile.transform.position = new Vector3 (i, 0, j);
+			}
+		}
+		for (int zplace = -1; zplace <= z + 1; zplace++) {
+			var outterTile = Instantiate (fieldSquare);
+			var renderer = outterTile.GetComponent<Renderer> ();
+			outterTile.transform.position = new Vector3 (-1f, 0f, zplace);
+			var scale = outterTile.transform.localScale;
+			scale.y = 5f / 2;
+			outterTile.transform.localScale = scale;
 
-            var otherOutterTile = Instantiate(fieldSquare);
-            otherOutterTile.transform.position = new Vector3(x, 0f, zplace);
-            var otherRenderer = otherOutterTile.GetComponent<Renderer>();
-            renderer.material.color = Color.black;
-            otherRenderer.material.color = Color.black;
-        }
+			var otherOutterTile = Instantiate (fieldSquare);
+			otherOutterTile.transform.position = new Vector3 (x, 0f, zplace);
+			var scaleTwo = otherOutterTile.transform.localScale;
+			scaleTwo.y = 5f / 2;
+			otherOutterTile.transform.localScale = scaleTwo;
 
-        for(int xplace = -1;xplace <x; xplace++)
-        {
-            var outTile = Instantiate(fieldSquare);
-            var outRenderer = outTile.GetComponent<Renderer>();
-            outTile.transform.position = new Vector3(xplace, 0f, -1f);
-            outRenderer.material.color = Color.black;
-            var outTile2 = Instantiate(fieldSquare);
-            var outRenderer2 = outTile2.GetComponent<Renderer>();
-            outTile2.transform.position = new Vector3(xplace, 0f, z+1);
-            outRenderer2.material.color = Color.black;
+			var otherRenderer = otherOutterTile.GetComponent<Renderer> ();
+			renderer.material.color = Color.black;
+			otherRenderer.material.color = Color.black;
+		}
 
-        }
-    }
+		for (int xplace = -1; xplace < x; xplace++) {
+			var outTile = Instantiate (fieldSquare);
+			var outRenderer = outTile.GetComponent<Renderer> ();
+			outTile.transform.position = new Vector3 (xplace, 0f, -1f);
+
+			var scale = outTile.transform.localScale;
+			scale.y = 5f;
+			outTile.transform.localScale = scale;
+
+			outRenderer.material.color = Color.black;
+			var outTile2 = Instantiate (fieldSquare);
+
+			var scaleTwo = outTile.transform.localScale;
+			scaleTwo.y = 5f;
+			outTile.transform.localScale = scaleTwo;
+
+			var outRenderer2 = outTile2.GetComponent<Renderer> ();
+			outTile2.transform.position = new Vector3 (xplace, 0f, z + 1);
+			outRenderer2.material.color = Color.black;
+
+		}
+	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 }
