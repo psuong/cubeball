8ad59b3f4f9ad7ea6f336b0a1e13732fd1a403fe
Claudia Yan <claudiab.yan@gmail.com>
2016-10-15 00:05:24 -0400

    moved timeUp event from timer to DetermineWinner.cs. Added in functionality to change a UI Text with the winning team

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -4,24 +4,11 @@ using System.Collections;
 public class GameTimer : MonoBehaviour {
     [SerializeField]
     public float timer = 0f;
-    public float roundTime = 180f;
-
-    public delegate void TimeUpHandler();
-    public static event TimeUpHandler TimeUpEvent;
-
 
 	// Use this for initialization
 	void Start () {
         StartCoroutine("gameTimer");
 	}
-
-    public static void InvokeTimesUp()
-    {
-        if(TimeUpEvent != null)
-        {
-            TimeUpEvent();
-        }
-    }
 
     private IEnumerator gameTimer()
     {
@@ -29,12 +16,5 @@ public class GameTimer : MonoBehaviour {
         timer += 1;
     }
 	
-	// Update is called once per frame
-	void Update () {
-        if (timer == roundTime)
-        {
-            InvokeTimesUp();
-        }
-	
-	}
+
 }
