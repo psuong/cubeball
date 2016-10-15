7eecfd66a67758662b318e38187161ee860aa0c7
Claudia Yan <claudiab.yan@gmail.com>
2016-10-15 12:06:40 -0400

    Added Moveplayer function

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PlayerMovement : MonoBehaviour {
+    private bool[,] fieldMatrix;
+
 
 	// Use this for initialization
 	void Start () {
-	
+        fieldMatrix = GetComponent<FieldMatrix>().playingfield;
 	}
+
+    private void MovePlayer(string instruction)
+    {
+        var goalPosition = transform.position + GetOffsetVector(instruction);
+        try
+        {
+            if(fieldMatrix[(int)goalPosition.x,(int)goalPosition.z] == false)
+            {
+                fieldMatrix[(int)transform.position.x, (int)transform.position.z] = true;
+                transform.position = Vector3.MoveTowards(transform.position, goalPosition, 1f);
+                fieldMatrix[(int)transform.position.x, (int)transform.position.z] = false;
+            }
+            
+        }
+        catch(IndexOutOfRangeException)
+        {
+
+        }
+    }
 
     private Vector3 GetOffsetVector(string instruction)
     {
@@ -33,6 +55,8 @@ public class PlayerMovement : MonoBehaviour {
 
         return new Vector3(x, 0, z);
     }
+
+
 	
 	// Update is called once per frame
 	void Update () {
