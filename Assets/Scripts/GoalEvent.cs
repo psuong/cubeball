fd2fd768a5da66e10818d761ac55390c002c6f90
Porrith Suong <psuong95@gmail.com>
2016-10-15 10:57:10 -0400

    Implements the goal event.

--- Assets/Scripts/GoalEvent.cs
+++ Assets/Scripts/GoalEvent.cs
@@ -1,20 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[CreateAssetMenu(fileName = "Goal Event", menuName = "Scriptable Objects/Goal Event")]
 public class GoalEvent : ScriptableObject
 {
-	private delegate void OnGoalHandler(GameObject gameObject);
+	public delegate void OnGoalHandler(GameObject gameObject);
 	private event OnGoalHandler onGoalEvent;
 
-	public void Subscribe()
+	public void Subscribe(OnGoalHandler goalHandler)
 	{
+		onGoalEvent -= goalHandler;
+		onGoalEvent += goalHandler;
 	}
 
-	public void Unsubscribe()
+	public void Unsubscribe(OnGoalHandler goalHandler) 
 	{
+		onGoalEvent -= goalHandler;
 	}
 
 	public void Invoke(GameObject gameObject)
 	{
+		if (onGoalEvent != null)
+		{
+			onGoalEvent(gameObject);
+		}
 	}
 }
