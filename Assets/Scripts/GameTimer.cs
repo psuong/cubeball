1b1f59e79e1e3109b6557971dc7beb5a840eb574
Claudia Yan <claudiab.yan@gmail.com>
2016-10-15 12:36:07 -0400

    removed incorrect stuff from gametimer

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -2,40 +2,27 @@
 using System.Collections;
 
 public class GameTimer : MonoBehaviour {
-    [SerializeField]
-    public float timer = 0f;
     public float roundTime = 180f;
 
-    public delegate void TimeUpHandler();
-    public static event TimeUpHandler TimeUpEvent;
-
+    [SerializeField]
+    public float timer = 180f;
 
 	// Use this for initialization
 	void Start () 
 	{
+        timer = roundTime;
         StartCoroutine("gameTimer");
 	}
 
-    public static void InvokeTimesUp()
-    {
-        if(TimeUpEvent != null)
-        {
-            TimeUpEvent();
-        }
-    }
 
     private IEnumerator gameTimer()
     {
         yield return new WaitForSeconds(1f);
-        timer += 1;
+        timer -= 1;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (timer == roundTime)
-        {
-            InvokeTimesUp();
-        }
-	
+       
 	}
 }
